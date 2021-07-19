a6637b975c6ee4908a5ab3d6bdc3d109f494de5f
julou95 <louis.jungo@microspot.ch>
2021-07-19 16:21:08 +0200

    new styling

--- pages/games/index.tsx
+++ pages/games/index.tsx
@@ -1,4 +1,5 @@
-import Link from 'next/link'
+import Link from 'next/link';
+import styles from '../../styles/Games.module.css';
 import db from '../../utils/db';
 
 type Props = {
@@ -16,12 +17,16 @@ const Posts = (props: Props) => {
     <div>
       <h1>Spiele</h1>
       {entriesData.map(entry => (
-        <div key={entry.id}>
-          <Link href={`/games/${entry.id}`}>
-            <a>{entry.name}</a>
-          </Link>
-          <br />
-        </div>
+        <Link href={`/games/${entry.id}`} key={entry.id}>
+          <a className={styles.link}>
+            {entry.name}
+            <div className={styles.arrow}>
+              <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000">
+                <path d="M12 4l-1.41 1.41L16.17 11H4v2h12.17l-5.58 5.59L12 20l8-8z"/>
+              </svg>
+            </div>
+          </a>
+        </Link>
       ))}
     </div>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,36 @@
 import Link from 'next/link';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
-    <div>
-        <Link href="/games">GAMES -&gt;</Link>
+    <div className={styles.content}>
+        <Link href={"/games"}>
+            <a className={styles.card}>
+                <Image src="https://via.placeholder.com/500x200" width={500} height={200} alt="Talisman" />
+                <div className={styles.cardContent}>
+                    <h3>Brettspiele</h3>
+                    <div className={styles.arrow}>
+                        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000">
+                            <path d="M12 4l-1.41 1.41L16.17 11H4v2h12.17l-5.58 5.59L12 20l8-8z"/>
+                        </svg>
+                    </div>
+                </div>
+            </a>
+        </Link>
+        {/*<Link href={"/games"}>*/}
+        {/*    <a className={styles.card}>*/}
+        {/*        <Image src="https://via.placeholder.com/500x200" width={500} height={200} alt="Talisman" />*/}
+        {/*        <div className={styles.cardContent}>*/}
+        {/*            <h3>Brettspiele</h3>*/}
+        {/*            <div className={styles.arrow}>*/}
+        {/*                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000">*/}
+        {/*                    <path d="M12 4l-1.41 1.41L16.17 11H4v2h12.17l-5.58 5.59L12 20l8-8z"/>*/}
+        {/*                </svg>*/}
+        {/*            </div>*/}
+        {/*        </div>*/}
+        {/*    </a>*/}
+        {/*</Link>*/}
     </div>
   )
 }
