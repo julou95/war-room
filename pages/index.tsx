e9e59a3c5a2cda8c28ff392a8a4f509ca9864847
julou95 <louis.jungo@microspot.ch>
2021-07-19 16:27:21 +0200

    type script support

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <div className={styles.content}>
         <Link href={"/games"}>
             <a className={styles.card}>
-                <Image src="https://via.placeholder.com/500x200" width={500} height={200} alt="Talisman" />
+                <Image src="https://via.placeholder.com/500x200" width={600} height={200} alt="Talisman" />
                 <div className={styles.cardContent}>
                     <h3>Brettspiele</h3>
                     <div className={styles.arrow}>
@@ -18,19 +18,6 @@ export default function Home() {
                 </div>
             </a>
         </Link>
-        {/*<Link href={"/games"}>*/}
-        {/*    <a className={styles.card}>*/}
-        {/*        <Image src="https://via.placeholder.com/500x200" width={500} height={200} alt="Talisman" />*/}
-        {/*        <div className={styles.cardContent}>*/}
-        {/*            <h3>Brettspiele</h3>*/}
-        {/*            <div className={styles.arrow}>*/}
-        {/*                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000">*/}
-        {/*                    <path d="M12 4l-1.41 1.41L16.17 11H4v2h12.17l-5.58 5.59L12 20l8-8z"/>*/}
-        {/*                </svg>*/}
-        {/*            </div>*/}
-        {/*        </div>*/}
-        {/*    </a>*/}
-        {/*</Link>*/}
     </div>
   )
 }
